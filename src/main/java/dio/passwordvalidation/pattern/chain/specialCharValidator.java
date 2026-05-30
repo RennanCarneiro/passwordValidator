86c65178f52e9efb22e919e0f0415ff1331b154b
Rennan <rennanbarros10@gmail.com>
2026-05-30 15:53:56 -0300

    refactor: implement specialCharValidator class extending passwordValidator and add validate method

--- src/main/java/dio/passwordvalidation/pattern/chain/specialCharValidator.java
+++ src/main/java/dio/passwordvalidation/pattern/chain/specialCharValidator.java
@@ -1,5 +1,13 @@
 package main.java.dio.passwordvalidation.pattern.chain;
 
-public class specialCharValidator {
+public class specialCharValidator extends passwordValidator{
+
+    @Override
+    public void validate(String password) {
+        // regex para buscar pelo menos um caractere especial
+        if (!password.matches(".*[!@#$%^&*(),.?\":{}|<>].*"))
+            throw new UnsupportedOperationException("A senha deve conter pelo menos um caractere especial.");
+        checkNext(password);
+    }
     
 }
