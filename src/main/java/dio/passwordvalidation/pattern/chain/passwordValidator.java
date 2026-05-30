7b495fe826607a748af1ca74bdc3d6de3a6aeffc
Rennan <rennanbarros10@gmail.com>
2026-05-30 15:42:22 -0300

    refactor: change passwordValidator class to abstract and implement method validate and checkNext

--- src/main/java/dio/passwordvalidation/pattern/chain/passwordValidator.java
+++ src/main/java/dio/passwordvalidation/pattern/chain/passwordValidator.java
@@ -1,7 +1,20 @@
 package main.java.dio.passwordvalidation.pattern.chain;
 
-public class passwordValidator {
+public abstract class passwordValidator {
     private passwordValidator nextValidator;
 
-    
+    //metodo para ligar um ao outro
+    public passwordValidator linkWith(passwordValidator nexValidator){
+        this.nextValidator = nexValidator;
+        return nexValidator;
+    }
+
+    //cada validador terá esse metodo para implementar
+    public abstract void validate(String password);
+
+    //passa o comando para o proximo da fila
+    protected void checkNext(String password){
+        if (nextValidator != null)
+            nextValidator.validate(password);
+    }
 }
