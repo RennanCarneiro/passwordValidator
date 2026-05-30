0c94689d8f871549cad0ad4b1c53f4d4e02214bb
Rennan <rennanbarros10@gmail.com>
2026-05-30 15:55:37 -0300

    refactor: initialize passwordValidator chain in passwordValidationService constructor

--- src/main/java/dio/passwordvalidation/service/passwordValidationService.java
+++ src/main/java/dio/passwordvalidation/service/passwordValidationService.java
@@ -3,5 +3,12 @@ package main.java.dio.passwordvalidation.service;
 import main.java.dio.passwordvalidation.pattern.chain.*;
 
 public class passwordValidationService {
-    
+
+    private final passwordValidator validatorChain;
+
+    public passwordValidationService() {
+        // Tamanho -> Maiúscula -> Especial
+        this.validatorChain = new lengthValidator();
+        this.validatorChain.linkWith(new upperCaseValidator()).linkWith(new specialCharValidator()); 
+    }
 }
