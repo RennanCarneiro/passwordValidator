bb52358a07106abed8f9e64862c7744b2665ea05
Rennan <rennanbarros10@gmail.com>
2026-05-30 17:27:43 -0300

    fix: correct password length validation to require a minimum of 8 characters

--- src/main/java/dio/passwordValidation/pattern/chain/LengthValidator.java
+++ src/main/java/dio/passwordValidation/pattern/chain/LengthValidator.java
@@ -3,8 +3,8 @@ package dio.passwordValidation.pattern.chain;
 public class LengthValidator extends PasswordValidator {
     @Override
     public void validate(String password){
-        if(password == null || password.length() < 0)
-            throw new IllegalArgumentException("A senha deve conter pelo menos 8 caracteres.");
+        if(password == null || password.length() < 8)
+            throw new UnsupportedOperationException("A senha deve conter pelo menos 8 caracteres.");
         checkNext(password);
     }
 }
