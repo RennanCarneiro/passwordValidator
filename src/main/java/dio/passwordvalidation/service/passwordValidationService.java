da5ea5eef6901641cce2a4f8ba204439880b894f
Rennan <rennanbarros10@gmail.com>
2026-05-30 15:57:37 -0300

    refactor: add processPassword method to passwordValidationService for password validation

--- src/main/java/dio/passwordvalidation/service/passwordValidationService.java
+++ src/main/java/dio/passwordvalidation/service/passwordValidationService.java
@@ -11,4 +11,11 @@ public class passwordValidationService {
         this.validatorChain = new lengthValidator();
         this.validatorChain.linkWith(new upperCaseValidator()).linkWith(new specialCharValidator()); 
     }
+
+    public String processPassword(String password){
+        //validação falha -> exceção lançada e corta o fluxo
+        validatorChain.validate(password);
+        String mensagem = "Senha validada com sucesso. Nível de segurança adequado.";
+        return mensagem;
+    }
 }
